c0834b28cf338f72c7a64f061ea057a1d2caa3fb
killiangr <killian.graindorge@efrei.net>
2022-10-02 12:05:06 +0200

    Set basic Components for React App

---
+++ space_shop/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Les Robots rêvent ils de moutons électriques</p>
+    </div>
+  );
+}
+
+export default App;
